9cba455077df7f56ac9a5eb2850800bb152184d0
Josh Mabry <mabry1985@gmail.com>
2020-05-20 11:33:28 -0700

    add train and world assets

---
+++ Assets/Scripts/AI/Train/Train.cs
@@ -0,0 +1,21 @@
+﻿using PathCreation;
+using UnityEngine;
+
+public class Train : MonoBehaviour
+{
+    public PathCreator pathCreator;
+    public float speed = 5;
+    float distanceTravelled;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+        transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);        
+    }
+}
