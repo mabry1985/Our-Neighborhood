c9c81927b13824f4c021ad8bf0540115c0e88c6c
Josh Mabry <mabry1985@gmail.com>
2020-05-20 15:54:18 -0700

    update material

--- Assets/Scripts/AI/Train/TrainWheels.cs
+++ Assets/Scripts/AI/Train/TrainWheels.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class TrainWheels : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.rotation()
     }
 }
