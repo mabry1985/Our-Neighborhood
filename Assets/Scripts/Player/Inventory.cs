643b71a1eb3ac9688862ff781fa9d1245eb0a644
Josh Mabry <mabry1985@gmail.com>
2020-05-20 21:43:52 -0700

    add station and tunnel models

--- Assets/Scripts/Player/Inventory.cs
+++ Assets/Scripts/Player/Inventory.cs
@@ -9,10 +9,10 @@ public class Inventory : MonoBehaviour {
 	public int drawOffset = 0;
 	public string name = "";
 
-	void OnGUI()
-	{
-		GUI.Box(new Rect(0, 0 + drawOffset, 100, 100), "" + name);
-		GUI.Label(new Rect(10, 20 + drawOffset, 100, 20), "Flour: " + flourLevel);
-		GUI.Label(new Rect(10, 35 + drawOffset, 100, 20), "Bread: " + breadLevel);
-	}
+	// void OnGUI()
+	// {
+	// 	GUI.Box(new Rect(0, 0 + drawOffset, 100, 100), "" + name);
+	// 	GUI.Label(new Rect(10, 20 + drawOffset, 100, 20), "Flour: " + flourLevel);
+	// 	GUI.Label(new Rect(10, 35 + drawOffset, 100, 20), "Bread: " + breadLevel);
+	// }
 }
