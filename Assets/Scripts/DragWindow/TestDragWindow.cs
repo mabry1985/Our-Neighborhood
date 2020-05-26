cc9a06b2d932fb60ed7d956252465013c3a0b820
Josh Mabry <mabry1985@gmail.com>
2020-05-26 11:37:26 -0700

    import LWRP, Shader graph, and terminal packages

---
+++ Assets/Scripts/DragWindow/TestDragWindow.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class TestDragWindow : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerDownHandler
+{
+
+    [SerializeField] private RectTransform dragRectTransform;
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private Image backgroundImage;
+    [SerializeField] private Color backgroundColor;
+
+
+
+    private void Awake()
+    {
+        if (dragRectTransform == null)
+        {
+            dragRectTransform = transform.parent.GetComponent<RectTransform>();
+        }
+
+        if(canvas == null)
+        {
+            Transform testCanvasTransform = transform.parent;
+            while (testCanvasTransform != null)
+            {
+                canvas = testCanvasTransform.GetComponent<Canvas>();
+                if (canvas != null)
+                {
+                    break;
+                }
+                testCanvasTransform = testCanvasTransform.parent;
+            }
+        }
+
+        if(backgroundImage == null)
+        {
+            Transform testBackgroundImage = transform.parent;
+            while (testBackgroundImage != null)
+            {
+                backgroundImage = testBackgroundImage.GetComponentInChildren<Image>();
+                if (backgroundImage != null)
+                {
+                    break;
+                }
+            }
+            backgroundColor = backgroundImage.color;
+        }
+
+        if(backgroundColor == null)
+        {
+            _ = backgroundImage.color;
+        }
+
+        
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        backgroundColor.a = .1f;
+        backgroundImage.color = backgroundColor;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        dragRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        backgroundColor.a = 1f;
+        backgroundImage.color = backgroundColor;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        dragRectTransform.SetAsLastSibling();
+    }
+}
---
+++ Assets/Scripts/Input_Window/Input_Manager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using CodeMonkey.Utils;
+
+//Input Manager contains 
+
+public class Input_Manager : MonoBehaviour
+{
+    private UI_Input_Window commandInputWindow;
+    private TMP_InputField inputField;
+
+    private List<string> input_Logs = new List<string>();
+
+    private bool windowBool = true;
+
+    #region RunTimeEvents
+    private void Awake()
+    {
+       if (commandInputWindow == null)
+        {
+            commandInputWindow = transform.Find("UI_Input_Window").GetComponent<UI_Input_Window>();
+        }
+
+       if (inputField == null)
+        {
+            inputField = transform.GetComponentInChildren<TMP_InputField>();
+        }
+    }
+
+    private void Start()
+    {
+        input_Logs = new List<string>();
+        
+    }
+    void Update()
+    {
+        OpenCommandWindow();
+        PrintCommandItems();
+        PrintCommandList();
+
+    }
+
+    #endregion
+
+    #region Private Methods
+    private void OpenCommandWindow()
+    {
+        if (Input.GetButtonDown("Input"))
+        {
+            if (windowBool)
+            {
+                UI_Input_Window.Static_ShowInputWindow("Type your commands here.", 
+                    //onCancel
+                    () => { UI_Input_Window.Static_HideInputWindow(); },
+                    //onOk
+                    (string inputText) => {input_Logs.Add(inputText);});
+
+                windowBool = !windowBool;
+            }
+            else
+            {
+                UI_Input_Window.Static_HideInputWindow();
+                windowBool = !windowBool;
+            }
+
+        }
+    }
+    #endregion
+
+    #region Public Methods
+    public void PrintCommandItems()
+    {
+        string temp = "";
+        foreach (string a in input_Logs)
+        {
+            temp += a + " ";
+            Debug.Log(temp);
+        }
+    }
+
+    public void PrintCommandByIndex(int i)
+    {
+        print(input_Logs[i]);
+    }
+
+    public List<string> PrintCommandList()
+    {
+        return input_Logs;
+    }
+
+    #endregion
+}
