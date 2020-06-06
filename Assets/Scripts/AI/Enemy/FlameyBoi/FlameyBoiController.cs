578bf04e522da53c75f3932190dc6a4ec51f6aeb
Josh Mabry <mabry1985@gmail.com>
2020-06-06 01:43:03 -0700

    refactor health component for enemies

--- Assets/Scripts/AI/Enemy/FlameyBoi/FlameyBoiController.cs
+++ Assets/Scripts/AI/Enemy/FlameyBoi/FlameyBoiController.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class FlameyBoiController : MonoBehaviour
 {
     [SerializeField] float chaseDistance = 5f;
     [SerializeField] float suspicionTime = 3f;
-    [SerializeField] PatrolPath patrolPath;
-    [SerializeField] float waypointTolerance = 3f;
-    [SerializeField] float waypointPauseTime = 3f;
+    // [SerializeField] PatrolPath patrolPath;
+    // [SerializeField] float waypointTolerance = 3f;
+    // [SerializeField] float waypointPauseTime = 3f;
+    [SerializeField] float wanderRadius = 10f;
+    [SerializeField] float wanderingPauseTime = 2f;
 
     Fighter fighter;
     GameObject[] players;
@@ -28,6 +31,8 @@ public class FlameyBoiController : MonoBehaviour
         mover = GetComponent<Mover>();
         flameParticle = GetComponentInChildren<ParticleSystem>();
         guardPosition = transform.position;
+
+        StartCoroutine(Wander());
     }
 
     private void Update()
@@ -44,6 +49,7 @@ public class FlameyBoiController : MonoBehaviour
 
         if (InAttackRange() && fighter.CanAttack(targetPlayer))
         {
+            StopCoroutine(Wander());
             AttackBehaviour();
         }
         else if (timeSinceLastSawPlayer < suspicionTime)
@@ -52,7 +58,6 @@ public class FlameyBoiController : MonoBehaviour
         }
         else
         {
-            PatrolBehaviour();
         }
 
         UpdateTimers();
@@ -64,40 +69,40 @@ public class FlameyBoiController : MonoBehaviour
         timeSinceArrivedAtWaypoint += Time.deltaTime;
     }
 
-    private void PatrolBehaviour()
-    {
-        Vector3 nextPosition = guardPosition;
+    // private void PatrolBehaviour()
+    // {
+    //     Vector3 nextPosition = guardPosition;
 
-        if (patrolPath != null)
-        {
-            if (AtWaypoint())
-            {
-                timeSinceArrivedAtWaypoint = 0;
-                CycleWaypoint();
-            }
-            nextPosition = GetCurrentWaypoint();
-        }
-        if(timeSinceArrivedAtWaypoint > waypointPauseTime)
-        {
-            mover.StartMoveAction(nextPosition);
-        }
-    }
+    //     if (patrolPath != null)
+    //     {
+    //         if (AtWaypoint())
+    //         {
+    //             timeSinceArrivedAtWaypoint = 0;
+    //             CycleWaypoint();
+    //         }
+    //         nextPosition = GetCurrentWaypoint();
+    //     }
+    //     if(timeSinceArrivedAtWaypoint > waypointPauseTime)
+    //     {
+    //         mover.StartMoveAction(nextPosition);
+    //     }
+    // }
 
-    private bool AtWaypoint()
-    {
-        float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
-        return distanceToWaypoint < waypointTolerance;
-    }
+    // private bool AtWaypoint()
+    // {
+    //     float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+    //     return distanceToWaypoint < waypointTolerance;
+    // }
 
-    private void CycleWaypoint()
-    {
-        currentWaypointIndex = patrolPath.GetNextIndex(currentWaypointIndex);
-    }
+    // private void CycleWaypoint()
+    // {
+    //     currentWaypointIndex = patrolPath.GetNextIndex(currentWaypointIndex);
+    // }
 
-    private Vector3 GetCurrentWaypoint()
-    {
-        return patrolPath.GetWaypoint(currentWaypointIndex);
-    }
+    // private Vector3 GetCurrentWaypoint()
+    // {
+    //     return patrolPath.GetWaypoint(currentWaypointIndex);
+    // }
 
     private void SuspicionBehaviour()
     {
@@ -135,9 +140,32 @@ public class FlameyBoiController : MonoBehaviour
         return bestTarget;
     }
 
+    IEnumerator Wander()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(wanderingPauseTime);
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            if (!GetComponent<Health>().IsDead())
+                GetComponent<NavMeshAgent>().SetDestination(newPos);
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = UnityEngine.Random.insideUnitSphere * dist;
+        randDirection += origin;
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+
     private void OnDrawGizmosSelected() 
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, wanderRadius);
     }
 }
--- Assets/Scripts/Core/Health.cs
+++ Assets/Scripts/Core/Health.cs
@@ -7,7 +7,7 @@ public class Health : MonoBehaviour
 {
     [SerializeField] float health = 100f;
     bool isDead = false;
-    //Player player;
+    Player player;
     NavMeshAgent navMeshAgent;
     Animator animator;
 
@@ -15,6 +15,7 @@ public class Health : MonoBehaviour
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
+        player = GetComponent<Player>();
     }
 
     public bool IsDead()
@@ -29,58 +30,119 @@ public class Health : MonoBehaviour
             Die();
     }
 
-    // private void Die()
-    // {
-
-    //     isDead = true;
-    //     GetComponent<Animator>().SetTrigger("die");
-    //     GetComponent<ActionScheduler>().CancelCurrentAction();
-    //     //GetComponent<CapsuleCollider>().enabled = false;
-
-    // }
-
     public void Die()
     {
         if (isDead) return;
-
-        //player.playerNameCanvas.enabled = false;
         isDead = true;
         GetComponent<ActionScheduler>().CancelCurrentAction();
         navMeshAgent.enabled = false;
         animator.enabled = false;
 
-        if (this.tag == "Streamer")
-        {
-            //var cam = player.playerController.vcam;
-            //cam.m_Follow = null;       
-        }
+        // if (this.tag == "Streamer")
+        // {
+        //     //var cam = player.playerController.vcam;
+        //     //cam.m_Follow = null;       
+        // }
 
-        if (this.tag == "Player")
-        {
-            Invoke("OnRevive", 10);
-        }
+        if (player == null) return;
+        
+        Invoke("OnRevive", 10);
+        
+        Canvas nameCanvas = player.playerNameCanvas ?? null;
+
+        if (nameCanvas == null) return;
+
+        nameCanvas.enabled = false;
     }
 
     public void OnRevive()
     {
-        Player player = GetComponent<Player>();
-        if(player == null ) return;
-
         navMeshAgent.transform.position = player.home.position;
-        
         isDead = false;
-        //player.playerNameCanvas.enabled = true;
         navMeshAgent.enabled = true;
         navMeshAgent.isStopped = false;
         animator.enabled = true;
-
         
 
-        if (this.tag == "Streamer")
-        {
-            //var cam = player.playerController.vcam;
-            // cam.m_Follow = this.transform;
-        }
+        // if (this.tag == "Streamer")
+        // {
+        //     //var cam = player.playerController.vcam;
+        //     // cam.m_Follow = this.transform;
+        // }
+        Canvas nameCanvas = player.playerNameCanvas;
+
+        if (nameCanvas == null) return;
+
+        nameCanvas.enabled = true;
 
     }
 }
+
+// public class Health : MonoBehaviour
+// {
+//     [SerializeField] float health = 100f;
+//     float originalHealth;
+//     bool isDead = false;
+//     Player player;
+//     NavMeshAgent navMeshAgent;
+//     Animator animator;
+
+//     private void Start()
+//     {
+//         player = GetComponent<Player>();
+//         originalHealth = health;
+//         navMeshAgent = GetComponent<NavMeshAgent>();
+//         animator = GetComponent<Animator>();
+//     }
+
+//     public bool IsDead()
+//     {
+//         return isDead;
+//     }
+
+//     public void TakeDamage(float damage)
+//     {
+//         health = Mathf.Max(health - damage, 0);
+//         if (health == 0)
+//             Die();
+//     }
+
+//     public void Die()
+//     {
+//         if (isDead) return;
+
+//         isDead = true;
+
+//         if (player == null) return;
+//         GetComponent<ActionScheduler>().CancelCurrentAction();
+//         navMeshAgent.enabled = false;
+//         animator.enabled = false;
+
+//         Invoke("Respawn", 10);
+
+//         Canvas nameCanvas = player.playerNameCanvas;
+
+//         if (nameCanvas == null) return;
+
+//         nameCanvas.enabled = false;
+
+//     }
+
+//     public void Respawn()
+//     {
+
+//         navMeshAgent.transform.position = player.home.position;
+
+//         isDead = false;
+//         navMeshAgent.enabled = true;
+//         navMeshAgent.isStopped = false;
+//         animator.enabled = true;
+
+//         Canvas nameCanvas = player.playerNameCanvas;
+
+//         if (nameCanvas == null) return;
+
+//         nameCanvas.enabled = true;
+
+//     }
+// }
