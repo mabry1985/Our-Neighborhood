c28c2a46265dab10df1f0134312b87e8f331a0d8
Josh Mabry <mabry1985@gmail.com>
2020-06-19 11:25:36 -0700

    add well and windmill to buid system

---
+++ Assets/Building/Scripts/BuildingScripts/Windmill.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Windmill : MonoBehaviour
+{
+    private void Start() 
+    {
+
+    }
+
+    public void OnPlacement()
+    {
+        transform.GetChild(0).gameObject.SetActive(true);
+    }
+}
